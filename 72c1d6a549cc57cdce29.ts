3573ecd0ec7d23b7faa908b4b054c974d37ee21a
Artyom445work <Artyom445work@users.noreply.github.com>
2025-01-29 19:44:50 +0000

    deploy: bf757813769ba2ef51a4c8b26d443db90c73765f

---
+++ 72c1d6a549cc57cdce29.ts
@@ -0,0 +1,6 @@
+import { Chat } from './chat';
+import './style.css';
+const container = document.getElementById('app');
+if (container) {
+  new Chat(container);
+}
--- index.ts
+++
@@ -1,7 +0,0 @@
-import { Chat } from './chat'
-import './style.css'
-
-const container = document.getElementById('app')
-if (container) {
-    new Chat(container)
-}
