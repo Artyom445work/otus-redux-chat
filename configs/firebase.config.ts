1c4d992c123a2f5e72b9997a3f5286fb120aba25
Artyom445work <Artyom445work@users.noreply.github.com>
2025-01-29 16:02:53 +0000

    Deploying to gh-pages from @ Artyom445work/otus-redux-chat@a798eb7793959671ab116b0140605b2574229228 🚀

---
+++ configs/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBkYngop1w68t1YvjWV3K6e8579aaLwlAA",
+    authDomain: "otus-calendar-435fe.firebaseapp.com",
+    databaseURL: "https://otus-calendar-435fe-default-rtdb.firebaseio.com",
+    projectId: "otus-calendar-435fe",
+    storageBucket: "otus-calendar-435fe.firebasestorage.app",
+    messagingSenderId: "1064378256116",
+    appId: "1:1064378256116:web:090dba2f460758dfb16ca1",
+    measurementId: "G-ENP28Q1CXK"
+}
